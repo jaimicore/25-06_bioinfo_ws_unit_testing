b9011953882ad7705543758bf5c75163596fee03
Jaime Castro-Mondragon <j.a.castro.mondragon@gmail.com>
2025-06-02 16:08:33 +0000

    test: Added test with exception

--- second/tests/second_test.py
+++ second/tests/second_test.py
@@ -1,4 +1,6 @@
 from second.second import greeting
+import pytest
+
 
 def test_greeting_eng():
     assert greeting("James", "English") == "Hello James!"
@@ -15,3 +17,14 @@ def test_greeting_default():
 # continue by using assert
 # add your changes: git add second/tests/second_test.py
 # commit your changes using commit message conventions (https://inpred.github.io/24-03_bioinfo_ws/#19): git commit -m "test: <your commit message>"
+
+
+# We were expecting a type error here 1 as numeric value instead of a string
+# As this error is correct, the test pass the test, even if the function is incorrect
+def test_greeting_english():
+    with pytest.raises(TypeError):
+        assert greeting(1, "english") == "Hello Jaime!"
+
+
+# def test_greeting_English_numnamne():
+#     assert greeting(12, "English") == "Hello 1!"
