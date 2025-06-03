e4f59405fd4a6f03ad892b9550e7172df6d25f28
Jaime Castro-Mondragon <j.a.castro.mondragon@gmail.com>
2025-06-03 08:33:06 +0000

    fix: Commented not required test

--- fourth/fourth_test.py
+++ fourth/fourth_test.py
@@ -1,19 +1,20 @@
-from fourth import calculator,read_yaml
+from fourth.fourth import calculator,read_yaml
 import pytest
 from contextlib import nullcontext
 
 @pytest.mark.parametrize(
-    "x, y, operator,exception, want",
+    "x, y, operator, exception, want",
     [
-        (1, 2, "+", nullcontext(), 3),
-        (2, 1, "-", nullcontext(), 1),
-        (2, 3, "*", nullcontext(), 6),
-        (10, 5, "*", nullcontext(), 2),
-        ("1", "1", "*", pytest.raises(TypeError), None)
+        (1, 2, "addition", nullcontext(), 3),
+        (2, 1, "subtraction", nullcontext(), 1),
+        (2, 3, "multiplication", nullcontext(), 6),
+        (10, 5, "division", nullcontext(), 2),
+        ("1", "1", "addition", pytest.raises(TypeError), None)
+#        (10, 5, "/", pytest.raises(TypeError), pytest.raises(TypeError))
 
     ]
 )
 
-def test_calculator(x, y, exception, want):
+def test_calculator(x, y, operator, exception, want):
     with exception:
-        assert calculator(x, y) == want
+        assert calculator(x, y, operator) == want
